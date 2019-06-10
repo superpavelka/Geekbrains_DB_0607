3e4f1b29184b3c673834260c7a2823bee53a7664
krilatiy <pos@list.ru>
2019-06-10 15:34:42 +0300

    все задания в одном файле с комментариями

---
+++ task_1_1.sql
@@ -0,0 +1,26 @@
+-- # запускаю такой скрипт
+CREATE DATABASE IF NOT EXISTS example;
+USE example;
+CREATE TABLE IF NOT EXISTS users (
+id INT NOT NULL PRIMARY KEY, 
+name VARCHAR(30) NOT NULL);
+--# несколько записей для наглядности
+INSERT INTO users VALUES(001, 'John Doe');
+INSERT INTO users VALUES(002, 'Chris White');
+INSERT INTO users VALUES(003, 'Sam Smith');
+SELECT * FROM users;
+quit
+-- # это уже делаю в консоли
+mysqldump example > dump_example.sql;
+--# запускаю утилиту mysql
+mysql
+--# cоздаю новую БД sample
+CREATE DATABASE IF NOT EXISTS sample;
+--# выхожу из mysql
+quit
+--# разворачиваю туда дамп
+mysql sample < dump_example.sql;
+--# делаю дамп таблицы help_keyword из mysql
+mysqldump mysql help_keyword --where="true limit 100"> help_100.sql
+--# смотрю что получилось
+cat help_100.sql
