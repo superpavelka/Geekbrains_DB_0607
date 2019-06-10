2aa4778f84cb6615654ec022b00370dd94513c19
krilatiy <pos@list.ru>
2019-06-10 14:29:38 +0300

    my.cnf файл + скрипт создающий таблицу + файл дампа sql

---
+++ create_table_users.sql
@@ -0,0 +1,5 @@
+USE example;
+CREATE TABLE users (id INT, name VARCHAR(20));
+INSERT INTO users VALUES(001, 'John Doe');
+INSERT INTO users VALUES(002, 'Chris White');
+INSERT INTO users VALUES(003, 'Sam Smith');
