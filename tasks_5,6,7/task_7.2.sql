3b99e1600580bc37b6cf915730a1b60578672bdc
krilatiy <pos@list.ru>
2019-06-26 11:19:17 +0300

    7.2

---
+++ tasks_5,6,7/task_7.2.sql
@@ -0,0 +1,26 @@
+-- (по желанию) Пусть имеется таблица accounts содержащая три столбца id, name, password, содержащие первичный ключ,
+-- имя пользователя и его пароль. Создайте представление username таблицы accounts, предоставляющий доступ к столбцам 
+-- id и name. Создайте пользователя user_read, который бы не имел доступа к таблице accounts, однако, мог бы извлекать 
+-- записи из представления username.
+
+DROP DATABASE IF EXISTS users_accounts;
+CREATE DATABASE users_accounts;
+USE users_accounts;
+DROP TABLE IF EXISTS accounts;
+CREATE TABLE accounts
+(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    pwd VARCHAR(255)
+);
+
+CREATE VIEW username AS 
+(
+    SELECT id, name
+    FROM accounts
+);
+DROP USER IF EXISTS 'user_read'@'localhost';
+CREATE USER 'user_read'@'localhost' IDENTIFIED BY '=!=npuBETMuP=!=';
+
+GRANT USAGE ON *.* TO user_read;
+GRANT SELECT ON users_accounts.username TO user_read;
