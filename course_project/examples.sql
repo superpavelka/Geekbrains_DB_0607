435bf96bad1300f3e4b9ca88490ec7899c1aabd7
krilatiy <pos@list.ru>
2019-07-08 15:16:34 +0300

    course_project представление product_types_products + выборка : все футболки размера L фирмы Baon

--- course_project/examples.sql
+++ course_project/examples.sql
@@ -6,7 +6,10 @@ SELECT supplies.store, exchange_rates.`data`, exchange_rates.exchange_rate
    JOIN exchange_rates
  ON supplies.exchange_rate = exchange_rates.id_exchange_rate
  ORDER BY supplies.store;
- 
+
+-- Представление ЗАКУПКИ-МАГАЗИНЫ объединяющее колонки: 
+-- МАГАЗИНЫ: Id магазина, Адрес магазина, 
+-- ЗАКУПКИ: Id закупки, магазин закупки, курс закупки
 DROP VIEW IF EXISTS supplies_stores;
 CREATE VIEW supplies_stores AS 
  SELECT stores.id_store, stores.address, supplies.id_supply, supplies.store, supplies.exchange_rate 
@@ -18,3 +21,21 @@ SELECT supplies_stores.address, exchange_rates.`data`, exchange_rates.exchange_r
    JOIN exchange_rates
  ON supplies_stores.exchange_rate = exchange_rates.id_exchange_rate
  ORDER BY exchange_rates.`data`;
+
+-- Представление ТИПЫ ПРОДУКТОВ-ПРОДУКТЫ объединяющее колонки: 
+-- ТИПЫ ПРОДУКТОВ: Id типа продукта, название типа продукта, 
+-- ПРОДУКТЫ Id продукта, тип продукта, производитель продукта, описание продукта
+DROP VIEW IF EXISTS product_types_products;
+CREATE VIEW product_types_products AS 
+ SELECT product_types.id_product_type, product_types.product_type_name, products.product_type, products.manufacturer, products.id_product,products.description
+ FROM product_types,products;
+
+-- Показать все футболки размера L фирмы Baon
+SELECT product_types_products.product_type_name, manufacturers.name, sizes.`size`, product_types_products.description
+  FROM product_types_products
+  	JOIN manufacturers
+  	JOIN sizes
+  ON product_types_products.manufacturer = manufacturers.id_manufacturer
+    WHERE (sizes.`size` = 'L') AND (product_types_products.product_type_name = 'T-shirts') AND (manufacturers.name = 'Baon');
+ 
+
